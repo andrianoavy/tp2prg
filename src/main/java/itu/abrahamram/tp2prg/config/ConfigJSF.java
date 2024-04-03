8d0c81e33fd42472492a88549c58e471db640cd7
andrianoavy <abrahamramilison+andrianoavy@gmail.com>
2024-04-03 20:05:44 +0300

    Fichiers de configuration

---
+++ src/main/java/itu/abrahamram/tp2prg/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package itu.abrahamram.tp2prg.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Classe de configuration JSF
+ * @author Abraham
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+	
+}
